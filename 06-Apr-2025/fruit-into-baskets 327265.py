5a9964f010bca62ff1f377493dbd8e45970e030d
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-04-06 16:41:48 +0300

    Created 'fruit-into-baskets' by A2SV Hub

---
+++ 06-Apr-2025/fruit-into-baskets 327265.py
@@ -0,0 +1,27 @@
+# Problem: fruit-into-baskets - https://leetcode.com/problems/fruit-into-baskets/
+
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        n = len(fruits)
+        basket = {}
+        left = 0
+        the_max = 0
+        the_sum = 0
+        for right, fruit in enumerate(fruits):
+            if fruit not in basket:
+                basket[fruit] = 0
+            basket[fruit] += 1
+            the_sum += 1
+            while len(basket) > 2:
+                left_fruit = fruits[left]
+                basket[left_fruit] -= 1
+                the_sum -= 1
+                if basket[left_fruit] == 0:
+                    del basket[left_fruit]
+                left += 1
+            the_max = max(the_max, the_sum)
+        return the_max
