16abbf212cf035edf1522cd4df64ee5369635da4
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-04-28 21:17:38 +0300

    Created 'E - NATOLI's String Quest' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/E - NATOLI's String Quest 344208.py
@@ -0,0 +1,22 @@
+# Problem: E - NATOLI's String Quest - https://codeforces.com/gym/605795/problem/E
+
+s = input()
+n = len(s)
+t = []
+u = []
+remain = [None] * n
+
+remain[-1] = s[-1]
+for i in range(n-2, -1, -1):
+    remain[i] = min(s[i], remain[i+1])
+ 
+i = 0
+while i<n or t:
+    if t and (i>=n or t[-1]<= remain[i]):
+        u.append(t.pop())
+
+    else:
+        t.append(s[i])
+        i += 1
+
+print("".join(u))
