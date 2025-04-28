8c52e3b735a6d7054ef1dd84e5fd0bf8b45ecc6b
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-04-28 20:53:20 +0300

    Created 'D - Electric Mayhem' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/D - Electric Mayhem 344189.py
@@ -0,0 +1,13 @@
+# Problem: D - Electric Mayhem - https://codeforces.com/gym/605795/problem/D
+
+s = input()
+st = []
+for i in s:
+    if st and st[-1] == i:
+        st.pop()
+    else:
+        st.append(i)
+if st:
+    print("No")
+else:
+    print("Yes")
