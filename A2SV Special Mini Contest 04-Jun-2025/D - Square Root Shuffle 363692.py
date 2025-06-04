18cb790ef9190c07ab9cfa4fea574613f0278f5e
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-06-04 19:47:42 +0300

    Created 'D - Square Root Shuffle' by A2SV Hub

---
+++ A2SV Special Mini Contest 04-Jun-2025/D - Square Root Shuffle 363692.py
@@ -0,0 +1,18 @@
+# Problem: D - Square Root Shuffle - https://codeforces.com/gym/608569/problem/D
+
+for _ in range(int(input())):
+    n = int(input())
+    if n % 2 == 0:
+        arr = []
+        for i in range(n // 2, n // 2 + n + 1):
+            if i != n:
+                arr.append(i)
+        print(*arr)
+    else:
+        arr = []
+        for i in range(n // 2 + 3, n // 2 + 3 + n):
+            arr.append(i)
+        arr[0] -= 1
+        arr[-1] += 1
+        arr[-2] += 1
+        print(*arr)
