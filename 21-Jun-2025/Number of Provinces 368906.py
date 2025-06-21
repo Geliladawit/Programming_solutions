6d666bdef7ed93f6861de4b1565d2ad00d00972f
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-06-21 22:18:00 +0300

    Created 'Number of Provinces' by A2SV Hub

---
+++ 21-Jun-2025/Number of Provinces 368906.py
@@ -0,0 +1,19 @@
+# Problem: Number of Provinces - https://leetcode.com/problems/number-of-provinces/
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        visited = [False] * n
+        provinces = 0
+
+        def dfs(city):
+            for neighbor in range(n):
+                if isConnected[city][neighbor] == 1 and not visited[neighbor]:
+                    visited[neighbor] = True
+                    dfs(neighbor)
+        for i in range(n):
+            if not visited[i]:
+                provinces += 1
+                visited[i] = True
+                dfs(i)
+        return provinces
