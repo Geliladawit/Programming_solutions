1713fd7ab51f046490cdaa0ab511799f0a0ddd21
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-07-29 22:38:04 +0300

    Created 'Mod Power' by A2SV Hub

---
+++ 29-Jul-2025/Mod Power 380414.py
@@ -0,0 +1,12 @@
+# Problem: Mod Power - https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+result1 = pow(a, b)
+
+result2 = pow(a, b, m)
+
+print(result1)
+print(result2)
