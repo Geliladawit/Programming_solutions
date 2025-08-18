0cc2d92debd33fc6aa0c1b9253cdf501ff2b5aff
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-08-18 22:09:37 +0300

    Created 'A - Maxim vs. Yogurt: The Great Discount Battle' by A2SV Hub

---
+++ A2SV Contest #26 18-Aug-2025/A - Maxim vs. Yogurt: The Great Discount Battle 386630.py
@@ -0,0 +1,7 @@
+# Problem: A - Maxim vs. Yogurt: The Great Discount Battle - https://codeforces.com/gym/629689/problem/A
+
+t = int(input())
+for _ in range(t):
+    n, a, b = map(int, input().split())
+    amount = min(n * a, (n %2)* a + n//2 * b )
+    print(amount)
