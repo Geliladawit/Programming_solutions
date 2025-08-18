c728e2e0f04cd5c87e48ff5302a4a28c876de252
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-08-18 22:09:38 +0300

    Created 'B - Maxim vs. Letters: Newspaper Heist' by A2SV Hub

---
+++ A2SV Contest #26 18-Aug-2025/B - Maxim vs. Letters: Newspaper Heist 386631.py
@@ -0,0 +1,13 @@
+# Problem: B - Maxim vs. Letters: Newspaper Heist - https://codeforces.com/gym/629689/problem/B
+
+from collections import Counter
+
+h=input()
+t=input()
+f=Counter(h)
+ch=Counter(t)
+for i in ch:
+    if i != " " and ch[i] >f[i]:
+        print("NO")
+        exit()
+print("YES")
