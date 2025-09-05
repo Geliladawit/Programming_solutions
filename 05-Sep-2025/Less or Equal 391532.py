95a16183aa4a5f9975695f053730f8c822868642
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-09-05 23:44:46 +0300

    Created 'Less or Equal' by A2SV Hub

---
+++ 05-Sep-2025/Less or Equal 391532.py
@@ -0,0 +1,13 @@
+# Problem: Less or Equal - https://codeforces.com/problemset/problem/977/C
+
+n,k = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+res = -1
+if k == 0:
+    res = arr[0] - 1 if arr[0] > 1 else -1
+elif k == n:
+    res = arr[-1] 
+elif arr[k-1] != arr[k]:
+    res = arr[k - 1]
+print(res)
