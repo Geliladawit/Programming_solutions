16d0f442772de9140a081cc5d0afd1107cab51d2
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-09-06 21:41:37 +0300

    Created 'Gray Code' by A2SV Hub

---
+++ 06-Sep-2025/Gray Code 391660.py
@@ -0,0 +1,12 @@
+# Problem: Gray Code - https://leetcode.com/problems/gray-code/
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        def grayCodeSeq(n: int):
+            if n == 1:
+                return [0, 1]
+            prev = grayCodeSeq(n - 1)
+            res = prev[:]
+            res += [x | (1 << (n - 1)) for x in reversed(prev)]
+            return res
+        return grayCodeSeq(n)
