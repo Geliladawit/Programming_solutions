65623027380e2f66a2308196c17a6c09a12cdd77
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-09-06 21:10:03 +0300

    Created 'Remove Smallest' by A2SV Hub

---
+++ 06-Sep-2025/Remove Smallest 391654.py
@@ -0,0 +1,21 @@
+# Problem: Remove Smallest - https://codeforces.com/problemset/problem/1399/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    if n == 1:
+        print("YES")
+        continue
+
+    i = 0
+    while i < len(arr) - 1:
+        if abs(arr[i] - arr[i + 1]) <= 1:
+            arr.pop(i)
+        else:
+            i += 1
+    if len(arr) == 1:
+        print("YES")
+    else:
+        print("NO")
