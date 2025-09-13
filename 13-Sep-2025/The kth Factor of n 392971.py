f661dc2968e50eb7438b8c0d41a07993b4683015
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-09-13 19:33:39 +0300

    Created 'The kth Factor of n' by A2SV Hub

---
+++ 13-Sep-2025/The kth Factor of n 392971.py
@@ -0,0 +1,11 @@
+# Problem: The kth Factor of n - https://leetcode.com/problems/the-kth-factor-of-n/
+
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        res = []
+        for i in range(1,n + 1):
+            if n % i == 0:
+                res.append(i)
+            if len(res) == k:
+                return i
+        return -1
