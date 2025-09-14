7b95e951dfe697f2f522fd43ab313a5ac7d7c7ac
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-09-14 20:00:54 +0300

    Created 'C - Binary Deque' by A2SV Hub

---
+++ A2SV Contest #29 14-Sep-2025/C - Binary Deque 393109.py
@@ -0,0 +1,24 @@
+# Problem: C - Binary Deque - https://codeforces.com/gym/633600/problem/C
+
+t = int(input())
+for _ in range(t):
+    n, s = map(int, input().split())
+    arr = list(map(int, input().split()))
+    total = sum(arr)
+    if total < s:
+        print(-1)
+        continue
+    if total == s:
+        print(0)
+        continue
+
+    l, runnin_sum, m = 0, 0, -1
+    for r in range(n):
+        runnin_sum += arr[r]
+        while runnin_sum > s:
+            runnin_sum -= arr[l]
+            l += 1
+        if runnin_sum == s:
+            m = max(m, r - l + 1)
+
+    print(n - m if m != -1 else -1)
