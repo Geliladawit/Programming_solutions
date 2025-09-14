f73af8d993ce02ec5fad047d4b20493836706f69
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-09-14 20:01:06 +0300

    Created 'A - Zhan's Blender' by A2SV Hub

---
+++ A2SV Contest #29 14-Sep-2025/A - Zhan's Blender 393111.py
@@ -0,0 +1,9 @@
+# Problem: A - Zhan's Blender - https://codeforces.com/gym/633600/problem/A
+
+import math
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    x,y = map(int, input().split())
+    m = min(x,y)
+    print(math.ceil(n/m))
