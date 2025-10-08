d54baeb88a501ee8051bae570846f6046227a191
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2025-10-08 22:51:07 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 08-Oct-2025/Almost Prime 401227.py
@@ -0,0 +1,21 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+def prime_factors(num):
+    prime_factors_set = set()
+    d = 2
+    while d * d <= num:
+        while num % d == 0:
+            prime_factors_set.add(d)
+            num //= d
+        d += 1
+    if num > 1:
+        prime_factors_set.add(num)
+    return prime_factors_set
+n = int(input())
+cnt_almost_prime = 0
+for i in range(n + 1):
+    prime_factors_set = prime_factors(i)
+    if len(prime_factors_set) == 2:
+        cnt_almost_prime += 1
+
+print(cnt_almost_prime)
